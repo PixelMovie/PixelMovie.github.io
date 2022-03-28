e6cd50edc9b50d2ed2e4275d36b74d1a1d97994a
PixelMovie <yamitatsu760@gmail.com>
2022-03-28 10:31:20 +0200

    share result

--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -4,27 +4,40 @@ import '../css/EndGameModal.css';
 
 export default class EndGameModal extends Component {
 
+    copyToClipboard = (text) => {
+        const finalText = "https://pixelmovie.github.io\n\n" + text
+        navigator.clipboard.writeText(finalText).then(function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    }
+
     render() {
         const { tries, winTry } = this.props
+
+        const temp = [...Array(6)].map((element, i) => {
+            if (tries[i]) {
+                if (winTry - 1 === i) {
+                    return ":green_square:"// "🟩"//'\uD83D\uDFE9'
+                } else {
+                     return ":red_square:" //"🟥"//'\uD83D\uDFE5'
+                }
+            } else {
+                 return "⬜"
+            }
+        })
+        const resultString = "🎬" + temp.join("")
 
         return <div className="endGameContainer">
             <div className="endGameModal">
                 <h1>Partie Terminée</h1>
-                <p>Merci d'avoir jouer, à demain pour un nouveau film</p>
+                <p>Merci d'avoir jouer, à demain pour un nouveau film !</p>
                 <br />
-                <div>
-                    🎬&nbsp;{[...Array(6)].map((element, i) => {
-                        if (tries[i]) {
-                            if (winTry === i) {
-                                return '\uD83D\uDFE9'
-                            } else {
-                                 return '\uD83D\uDFE5'
-                            }
-                        } else {
-                             return "⬜"
-                        }
-                    })}
+                <div className="resultString">
+                    {resultString}
                 </div>
+                <button onClick={() => this.copyToClipboard(resultString)}>Partager</button>
             </div>
         </div>
     }
